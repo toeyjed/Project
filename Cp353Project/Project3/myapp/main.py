ff5ebda61b906b5afe6e45c42c4adbef815188f1
Jedsadaporn <jedsadaporn.toey@g.swu.ac.th>
2021-03-13 22:09:38 +0700

    add data

--- Cp353Project/Project3/myapp/main.py
+++ Cp353Project/Project3/myapp/main.py
@@ -23,14 +23,56 @@ def covid():
              'Hospitalized': response['Hospitalized'],
              'Died': response['Deaths']
              }
+    return render_template('covid19.html', covid=covid, user=current_user)
 
+
+@main.route('/news')
+def news():
+    URL = 'https://covid19.th-stat.com/api/open/cases/sum'
+    response = requests.get(URL).json()
+    casesum = ""
+    casesum = {'Province': response['Province']
+    
+    }
+    
+    return render_template('news.html',casesum=casesum , user=current_user)
+
+
+@main.route('/world')
+def world():
+    return render_template('covidworld.html', user=current_user)
+
+
+@main.route('/date')
+def covid_date():
     URL = "https://covid19.th-stat.com/api/open/timeline"
     response = requests.get(URL).json()
     data = response['Data']
-    date=[]
-    confirm=[]
-
+    date = []
+    confirm = []
+    recovered = []
+    hospitalized = []
+    deaths = []
     for i in range(len(data)):
+        data_date = data[i]
+        date.append(data_date['Date'])
+        confirm.append(data_date['Confirmed'])
+        recovered.append(data_date['Recovered'])
+        hospitalized.append(data_date['Hospitalized'])
+        deaths.append(data_date['Deaths'])
+    mylist = zip(date, confirm, recovered, hospitalized, deaths)
+    return render_template('covid_date.html', mylist=mylist, user=current_user)
+
+
+@main.route('/plot')
+def graph():
+    URL = "https://covid19.th-stat.com/api/open/timeline"
+    response = requests.get(URL).json()
+    data = response['Data']
+    date = []
+    confirm = []
+
+    for i in range(200, 400):
         data_date = data[i]
         date.append(data_date['Date'])
         confirm.append(data_date['Confirmed'])
@@ -48,51 +90,4 @@ def covid():
     ])
 
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template('covid19.html', covid=covid,plot=graphJSON, user=current_user)
-
-@main.route('/news')
-def news():
-    return render_template('news.html', user=current_user)
-    
-@main.route('/date')
-def covid_date():
-    URL = "https://covid19.th-stat.com/api/open/timeline"
-    response = requests.get(URL).json()
-    data = response['Data']
-    date = []
-    confirm = []
-    for i in range(len(data)):
-        data_date = data[i]
-        date.append(data_date['Date'])
-        confirm.append(data_date['Confirmed'])
-    mylist = zip(date, confirm)
-    return render_template('covid_date.html', mylist=mylist, user=current_user)
-
-
-# @main.route('/plot')
-# def graph():
-#     URL = "https://covid19.th-stat.com/api/open/timeline"
-#     response = requests.get(URL).json()
-#     data = response['Data']
-#     date=[]
-#     confirm=[]
-
-#     for i in range(200,400):
-#         data_date = data[i]
-#         date.append(data_date['Date'])
-#         confirm.append(data_date['Confirmed'])
-
-#     fig = go.Figure([
-#         go.Scatter(
-#             name='revenue',
-#             x=date,
-#             y=confirm,
-
-#             mode='lines',
-#             marker=dict(color='red', size=2),
-#             showlegend=True
-#         )
-#     ])
-
-#     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-#     return render_template('graph.html', plot=graphJSON, user=current_user)
+    return render_template('graph.html', plot=graphJSON, user=current_user)
